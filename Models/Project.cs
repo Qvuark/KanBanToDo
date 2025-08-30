31b19f322f67932044ebf5013c5753813767f006
Qvuark <roma18902015@gmail.com>
2025-08-30 12:05:43 +0300

    refactor: Restructure project into modular components

---
+++ Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanToDo.Models
+{
+    class Project
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public ObservableCollection<Task> Tasks { get; set; }
+        public Project() => Tasks = new ObservableCollection<Task>();
+    }
+}
